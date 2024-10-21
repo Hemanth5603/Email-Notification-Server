80bc89420536ea93ef108cc768d61499ad208f9e
Hemanth5603 <shemanth2003.vskp@gmail.com>
2024-10-21 20:09:02 +0530

    updated main

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/hemanth5603/email_notification_server/handlers"
 )
 
 func main() {
 	app := fiber.New()
-
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*", // You can specify specific origins here, e.g., "https://your-vercel-app.vercel.app"
+		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+	}))
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello")
 	})
